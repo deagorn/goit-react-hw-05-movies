c09dddf3789071980f7199a86e3bfdfa85cbd2f9
Nadiya Shrai <134630278+deagorn@users.noreply.github.com>
2024-03-05 11:29:45 +0200

    reviews

--- src/pages/MovieCard/MovieReviews.jsx
+++ src/pages/MovieCard/MovieReviews.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { fetchMovieReviews } from 'services/api';
+import styles from './MovieReviews.module.css';
 
 const MovieReviews = () => {
+const { movieId } = useParams();
+const [reviews, setReviews] = useState(null);
+  
+  useEffect(() => {
+    if (movieId) { 
+      fetchMovieReviews(movieId)
+        .then(data => setReviews(data))
+        .catch(err => console.log(err));
+    }
+  }, [movieId]);
+
+if (!reviews) {
+    return 'Loading...'
+  }
+
   return (
-    <div>MovieReviews</div>
+    <div>
+      <ul className={styles.reviews}>
+                {reviews.map(item => (
+                    <li key={item.id} className={styles.review}>
+                        <h2 className={styles.title}><span className={styles.name}>Name:</span> {item.author}</h2>
+                        <h3 className={styles.review_text}> {item.content}</h3>
+                    </li>
+                )
+                )}
+            </ul>
+    </div>
   )
 }
 
